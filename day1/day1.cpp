125c14ca46866fc1479ab215759033cbe926cd17
Johanna <152695438+Johanna1066@users.noreply.github.com>
2025-12-02 16:33:20 +0100

    algorithm for day 1 part 2

--- day1/day1.cpp
+++ day1/day1.cpp
@@ -3,60 +3,91 @@
 
 #include "day1.h"
 #include "../functions.h"
-#include <iostream>
 #include <fstream>
-#include <string>
+#include <iostream>
 #include <sstream>
+#include <string>
 #include <vector>
 
-void part1(std::vector<int>& instructions) {
-    
-    //Starting postition is 50
-    int position{50};
+void part1(std::vector<int> &instructions) {
+  // Starting postition is 50
+  int position{50};
 
-    //Counting how many times the dial is left pointing at zero
-    int sumZeroes{0};
+  // Counter for how many times the dial is left pointing at zero
+  int sumZeroes{0};
 
-    for (auto move : instructions){
-        position = (position + move) % 100;
-        if(position == 0) sumZeroes++;
-    }
+  for (auto move : instructions) {
+    position = ((position + move) % 100 + 100) % 100;
+    if (position == 0)
+      sumZeroes++;
+  }
 
-    std::cout << "Result part 1: " << sumZeroes << std::endl;
-
+  std::cout << "Result part 1: " << sumZeroes << std::endl;
 }
 
-void part2() {
+void part2(std::vector<int> &instructions) {
+  // Brute force approach
+
+  // Starting postition is 50
+  int position{50};
+
+  // Counter for how many times the dial passes zero
+  int sumZeroes{0};
+
+  for (auto move : instructions) {
+
+    const int next = position + move;
+
+    bool direction{true};
+    if (move < 0)
+      direction = false;
+
+    while (position != next) {
+      if (direction)
+        position++;
+      else
+        position--;
+
+      if (position % 100 == 0)
+        sumZeroes++;
+    }
+
+    position = ((position % 100) + 100) % 100;
+  }
+
+  std::cout << "Result part 2: " << sumZeroes << std::endl;
 }
 
-void day1()
-{
-    std::string fileURL = "../day1/inputDay1.txt";
-    std::ifstream input = readFile(fileURL);
+void day1() {
+  std::string fileURL = "../day1/inputDay1.txt";
+  std::ifstream input = readFile(fileURL);
 
-    std::string line;
-    std::vector<int> instructions;
+  std::string line;
+  std::vector<int> instructions;
 
-    while (std::getline(input, line)){
-        std::stringstream ss(line);
-        std::string token{};
+  // Reading and formating the data
+  while (std::getline(input, line)) {
+    std::stringstream ss(line);
+    std::string token{};
 
-        while (ss >> token) {
-            if (token.empty()) continue;
+    while (ss >> token) {
+      if (token.empty())
+        continue;
 
-            char turn = token[0];
-            std::size_t length = token.size();
-            int dist{};
-        
-            dist = std::stoi(token.substr(1,length));
+      char turn = token[0];
+      std::size_t length = token.size();
+      int dist{};
 
-            if (turn == 'L') dist = -dist;
+      dist = std::stoi(token.substr(1, length));
 
-            instructions.push_back(dist);
-        }
+      if (turn == 'L')
+        dist = -dist;
+
+      instructions.push_back(dist);
     }
-    input.close();
+  }
+  input.close();
 
-    part1(instructions);
-
+  part1(instructions);
+  part2(instructions);
 }
