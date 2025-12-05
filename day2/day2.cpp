b59213c3c6288d7fa341508beaa8604362f2171a
Johanna <152695438+Johanna1066@users.noreply.github.com>
2025-12-05 10:23:24 +0100

    algorithm day 2 part 1

--- day2/day2.cpp
+++ day2/day2.cpp
@@ -3,24 +3,91 @@
 
 #include "day2.h"
 #include "../functions.h"
+#include <cmath>
 #include <iostream>
+#include <sstream>
 #include <string>
+#include <vector>
 
-void part1() {}
+void part1(std::vector<std::string> &ranges) {
+  long sum{};
 
-void part2() {}
+  for (auto range : ranges) {
+
+    std::string firstStr = range.substr(0, range.find('-'));
+    std::string lastStr = range.substr(range.find('-') + 1);
+
+    long current{std::stol(firstStr)}, last{std::stol(lastStr)};
+
+    do {
+      long digits = (current == 0) ? 1 : (long)log10(current) + 1;
+
+      if (digits % 2 == 0) {
+
+        std::string currentStr = std::to_string(current);
+
+        std::string firstHalf = currentStr.substr(0, (digits / 2));
+        std::string secondHalf = currentStr.substr((digits / 2));
+
+        if (firstHalf == secondHalf) {
+          sum += current;
+        }
+      }
+
+      current++;
+
+    } while (current != (last + 1));
+  }
+  std::cout << "part 1: " << sum << std::endl;
+}
+
+void part2() { std::cout << "part 2: " << std::endl; }
 
 void day2() {
   std::string fileURL = "../day2/inputDay2.txt";
   std::ifstream input = readFile(fileURL);
 
   std::string line;
-  int sum1{}, sum2{};
+  std::vector<std::string> ranges;
 
   while (std::getline(input, line)) {
+
+    do {
+      size_t length = line.find_first_of(',');
+      std::string range{};
+
+      if (length == std::string::npos) {
+        // take the rest of the string
+        range = line;
+        line.clear();
+      } else {
+        range = line.substr(0, length);
+        line = line.substr(length + 1);
+      }
+
+      ranges.push_back(range);
+
+    } while (!line.empty());
+
+    /*while (!line.empty()) {
+
+      rangePair current;
+      std::string row{};
+
+      size_t length;
+      current.start = std::stoi(line, &length);
+      line = line.substr(length + 1);
+      current.finish = std::stoi(line, &length);
+      line = line.substr(length + 1);
+
+      std::cout << "start: " << current.start << "  finish: " <<
+    current.finish
+                << std::endl;
+
+      ranges.push_back(current);
+    }*/
+
+    part1(ranges);
   }
   input.close();
-
-  std::cout << "part 1: " << sum1 << std::endl;
-  std::cout << "part 2: " << sum2 << std::endl;
 }
