b78b4a894a310f7c96f0900d16a03d44b5ae5496
Johanna <152695438+Johanna1066@users.noreply.github.com>
2025-12-05 16:10:05 +0100

    algorithm for day 3 part 2, clean up and comment code

--- day3/day3.cpp
+++ day3/day3.cpp
@@ -11,47 +11,84 @@ void part1(std::vector<std::string> &joltages) {
   int sum{};
 
   for (auto joltage : joltages) {
-    size_t posFirst{};
+    size_t posFound{};
     std::string remainder{};
     std::string max{};
 
     for (char i : {'9', '8', '7', '6', '5', '4', '3', '2', '1', '0'}) {
-      posFirst = joltage.find(i);
+      posFound = joltage.find(i);
       size_t posLast = (joltage.length() - 2);
 
-      if (posFirst == posLast) {
+      if (posFound == posLast) {
         continue;
       }
-      if (posFirst != std::string::npos) {
+      if (posFound != std::string::npos) {
         max = i;
-        // std::cout << "max i 1: " << max << std::endl;
-        remainder = joltage.substr(posFirst + 1);
+        remainder = joltage.substr(posFound + 1);
         break;
       }
     }
-
-    // std::cout << "remainder: " << remainder << std::endl;
 
     for (char i : {'9', '8', '7', '6', '5', '4', '3', '2', '1', '0'}) {
-      posFirst = remainder.find(i);
+      posFound = remainder.find(i);
       size_t posLast = (joltage.length() - 1);
-      if (posFirst == posLast) {
+      if (posFound == posLast) {
         continue;
       }
-      if (posFirst != std::string::npos) {
+      if (posFound != std::string::npos) {
         max = max + i;
-        // std::cout << "max i 2: " << max << std::endl;
         break;
       }
     }
-    // std::cout << max << std::endl;
     sum += std::stoi(max);
   }
 
   std::cout << "Part 1: " << sum << std::endl;
 }
 
-void part2() {}
+// Part two is basically just a recursive version of part 1
+// part 1 could be solved with this function aswell. I would add one more
+// in-argument to decide how many digits to looks for and create the array
+// automatically from that argumen tinstead of manually as I've done in this
+// version
+
+void part2(std::vector<std::string> &joltages) {
+  long sum{};
+
+  for (auto joltage : joltages) {
+    size_t posFound{};
+    std::string remainder{};
+    remainder = joltage;
+    std::string max{};
+
+    // The request was to find the 'best' 12 digits to create the largest
+    // number.
+    for (int j : {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12}) {
+
+      // Checking through the whole line to see if there are any 9's, if not
+      // check for 8's and so on.
+      for (char i : {'9', '8', '7', '6', '5', '4', '3', '2', '1', '0'}) {
+        posFound = remainder.find(i);
+
+        // Making sure that the found digit isn't too close to the end.
+        size_t posLast = (remainder.length() - (14 - j));
+        if (posFound > posLast) {
+          continue;
+        }
+
+        // Adding the found digit to the number 'max'
+        if (posFound != std::string::npos) {
+          max = max + i;
+          remainder = remainder.substr(posFound + 1);
+          break;
+        }
+      }
+    }
+    sum += std::stol(max);
+  }
+
+  std::cout << "Part 2: " << sum << std::endl;
+}
 
 void day3() {
   std::string fileURL = "../day3/inputDay3.txt";
@@ -59,12 +96,13 @@ void day3() {
 
   std::string line;
   int sum1{}, sum2{};
-  std::vector<std::string> joltage{};
+  std::vector<std::string> joltages{};
 
   while (std::getline(input, line)) {
-    joltage.push_back(line);
+    joltages.push_back(line);
   }
   input.close();
 
-  part1(joltage);
+  part1(joltages);
+  part2(joltages);
 }
