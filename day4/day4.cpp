d447e3bcb3841c372eb9702e1b4eedf8dd331e96
Johanna <152695438+Johanna1066@users.noreply.github.com>
2025-12-06 12:06:35 +0100

    Set up for day 4

---
+++ day4/day4.cpp
@@ -0,0 +1,26 @@
+// day4.cpp
+// https://adventofcode.com/2025/day/4
+
+#include "day4.h"
+#include "../functions.h"
+#include <iostream>
+#include <string>
+
+void part1() {}
+
+void part2() {}
+
+void day4() {
+  std::string fileURL = "../day4/inputDay4.txt";
+  std::ifstream input = readFile(fileURL);
+
+  std::string line;
+  int sum1{}, sum2{};
+
+  while (std::getline(input, line)) {
+  }
+  input.close();
+
+  std::cout << "part 1: " << sum1 << std::endl;
+  std::cout << "part 2: " << sum2 << std::endl;
+}
--- main.cpp
+++ main.cpp
@@ -3,10 +3,11 @@
 
 // #include "day1/day1.h"
 // #include "day2/day2.h"
-#include "day3/day3.h"
+// #include "day3/day3.h"
+#include "day4/day4.h"
 #include "functions.h"
 
 int main() {
   // day
-  day3();
+  day4();
 }
