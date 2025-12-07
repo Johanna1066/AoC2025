d8b5aba7420194028186607749a3e7521c14e04e
Johanna <152695438+Johanna1066@users.noreply.github.com>
2025-12-07 17:54:14 +0100

    Algorithm for day 5 part 2

--- day5/day5.cpp
+++ day5/day5.cpp
@@ -36,8 +36,81 @@ void part1(std::vector<std::string> &ranges, std::vector<long> &productIDs) {
   std::cout << "part 1: " << sum << std::endl;
 }
 
-void part2(std::vector<std::string> &ranges) {
-  int sum{};
+void part2(std::vector<std::string> &rangesInput) {
+
+  // Ordering the ranges in vector of pairs
+  std::vector<std::pair<long, long>> ranges;
+
+  for (auto range : rangesInput) {
+    std::string temp{};
+    long first{}, last{};
+    size_t length;
+    first = std::stol(range, &length);
+    temp = range.substr(length + 1);
+    last = std::stol(temp, &length);
+
+    ranges.push_back({first, last});
+  }
+
+  // Sorting vector of pairs
+  std::sort(ranges.begin(), ranges.end());
+
+  // Vectors that will contain the final ranges. Could be made to one vector of
+  // pairs
+  std::vector<long> start{}, stop{};
+
+  for (auto range : ranges) {
+
+    // Making sure the range is from low to high. Otherwive it's an unvalid
+    // range and skip it
+    if ((range.first > range.second)) {
+      continue;
+    }
+
+    bool added{false};
+
+    for (int i = 0; i < start.size(); i++) {
+
+      // not overlapping with range i at all
+      if ((range.second < start[i]) || (range.first > stop[i])) {
+        continue;
+      }
+
+      // The current range is totally overlapped by range i
+      if (range.first >= start[i] && range.second <= stop[i]) {
+        added = true;
+        break;
+      }
+
+      // overlapping with range i on one or both sides
+      if (range.second >= stop[i]) {
+        stop[i] = range.second;
+        if (range.first < start[i]) {
+          start[i] = range.first;
+        }
+      }
+
+      if (range.first <= start[i]) {
+        start[i] = range.first;
+      }
+      added = true;
+      i = 0;
+    }
+
+    if (!added) {
+      start.push_back(range.first);
+      stop.push_back(range.second);
+    }
+  }
+
+  // Adding all the ranges. Adding one to each loop to include the whole range
+  // and not only the difference
+  long sum{};
+
+  for (int i = 0; i < start.size(); i++) {
+    sum += (stop[i] - start[i] + 1);
+  }
+
   std::cout << "part 2: " << sum << std::endl;
 }
 
